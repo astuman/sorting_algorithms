496ff4def9196a43a87753304fcaf81f1971eb59
Astewul <loloalex1992@gmail.com>
2022-08-16 12:01:12 -0300

    commiting

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -6,6 +6,7 @@
  *@list: node list
  *Return: return a pointer to a node which was enter it
  */
+
 listint_t *swap_node(listint_t *node, listint_t **list)
 {
 	listint_t *back = node->prev, *current = node;
